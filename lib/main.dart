ef54e56c92b34e357f466ba73e685356540bce80
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-03 09:15:39 -0500

    Second pass:
    This is code I copied in from website.
    This didn't work.
    
    Also: deleted Test directory; not using it and was show "Dart Analysis" problems.

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
-class PassedData with ChangeNotifier {
-  final String _data = 'New Passed Data From Top Level Widget';
-
-  String get data => _data;
-
-  void updateString() {
-    notifyListeners();
-  }
-
-}
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Provider<PassedData>(
-      create: (context) =>  PassedData(),
-      dispose: (context, value) => value.dispose(),
+    return ChangeNotifierProvider(
+      create: (context) => Data(),
       child: MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+        home: Scaffold(
+          appBar: AppBar(
+            title: MyText(),
+          ),
+          body: Level1(),
         ),
-        home: Level1(),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
-  const Level1({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(context.watch<PassedData>().data,),
-      ),
-      body: Level2(),
+    return Container(
+      child: Level2(),
     );
   }
 }
 
 class Level2 extends StatelessWidget {
-  const Level2({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Level3(),
+    return Column(
+      children: [
+        MyTextField(),
+        Level3(),
+      ],
     );
   }
 }
 
 class Level3 extends StatelessWidget {
-  const Level3({Key? key}) : super(key: key);
-
-
   @override
   Widget build(BuildContext context) {
-    return Text(context.watch<PassedData>().data,);
+    return Text(Provider.of(context).data);
   }
 }
 
+class MyText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of(context, listen: false).data);
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (newText) {
+        Provider.of(context, listen: false).changeString(newText);
+      },
+    );
+  }
+}
+
+class Data extends ChangeNotifier {
+  String data = 'Some data';
+
+  void changeString(String newString) {
+    data = newString;
+    notifyListeners();
+  }
+}
